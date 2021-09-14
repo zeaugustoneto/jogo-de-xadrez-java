1c47a811bf0b8d4628ea11ed02c6ee943c2f697a
José Augusto Neto <netiin00@gmail.com>
2021-09-14 15:26:54 -0300

    14/09/2021 Começando a implementar o tabuleiro e as peças

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,6 +5,8 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author zeneto
@@ -16,7 +18,8 @@ public class Program {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
     
 }
