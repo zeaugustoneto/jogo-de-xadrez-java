6feb882b1660313011f3a3ca71fc0dbc51c7917b
José Augusto Neto <netiin00@gmail.com>
2021-09-27 11:13:07 -0300

    27/09/2021 Colocando peças no tabuleiro

---
+++ src/main/java/chess/pieces/King.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author zeneto
+ */
+public class King extends ChessPiece{
+    
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    
+    @Override
+    public String toString(){
+        return "K";
+    }
+    
+    
+}
