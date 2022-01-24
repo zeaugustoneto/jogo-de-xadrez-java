9f1be0d58c464f944f1e763b2b0cbb03a87f030d
José Augusto Neto <netiin00@gmail.com>
2022-01-24 17:16:08 -0300

    24/01/2022 Tratando melhor a promoção de peças e correções de bugs.

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,3 @@
-
 package application;
 
 import java.util.ArrayList;
@@ -42,7 +41,11 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -9,45 +9,38 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-/**
- *
- * @author zeneto
- */
 public abstract class ChessPiece extends Piece {
-    private Color color;
-    private int moveCount;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	private Color color;
+	private int moveCount;
 
-    public Color getColor() {
-        return color;
-    }
-    public int getMoveCount(){
-        return moveCount;
-    }
-    
-    public void increaseMoveCount(){
-        moveCount++;
-    }
-    
-    public void decreaseMoveCount(){
-        moveCount--;
-    }
-    
-    public ChessPosition getChessPosition(){
-        return ChessPosition.fromPosition(position);
-        
-    }
-    
-    
-    
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p != null && p.getColor() != color;
-        
-    }
-               
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
+
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
